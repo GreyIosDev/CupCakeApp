044c94fbc1af8dc150fb9b4dadb9173bbbf0706c
NNADI HENRY UGONNA <greyios845@gmail.com>
2023-12-04 13:25:07 +0000

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Grey  on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
